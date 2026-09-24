cfd05c238914097b258316589fd7076e8e811a66
Samuel <samuelbird01@gmail.com>
2026-09-23 22:46:13 -0600

    StatusItemScroller is complete and functioning accurately - still too many posts?

--- src/components/authentication/OAuth.tsx
+++ src/components/authentication/OAuth.tsx
@@ -3,7 +3,6 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 interface Props {
     displayInfoMessageWithDarkBackground: (message: string) => void;
-
 }
 
 
--- src/components/mainLayout/FeedScroller.tsx
+++ src/components/mainLayout/FeedScroller.tsx
@@ -10,6 +10,7 @@ import { ToastActionsContext } from "../toaster/ToastContexts";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { ToastType } from "../toaster/Toast";
 import Post from "../statusItem/Post";
+import StatusItemScroller from "./StatusItemScroller";
 
 export const PAGE_SIZE = 10;
 
@@ -122,55 +123,13 @@ const FeedScroller = () => {
   };
 
   return (
-    <div className="container px-0 overflow-visible vh-100">
-      <InfiniteScroll
-        className="pr-0 mr-0"
-        dataLength={items.length}
-        next={() => loadMoreItems(lastItem)}
-        hasMore={hasMoreItems}
-        loader={<h4>Loading...</h4>}
-      >
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className="row mb-3 mx-0 px-0 border rounded bg-white"
-          >
-            <div className="col bg-light mx-0 px-0">
-              <div className="container px-0">
-                <div className="row mx-0 px-0">
-                  <div className="col-auto p-3">
-                    <img
-                      src={item.user.imageUrl}
-                      className="img-fluid"
-                      width="80"
-                      alt="Posting user"
-                    />
-                  </div>
-                  <div className="col">
-                    <h2>
-                      <b>
-                        {item.user.firstName} {item.user.lastName}
-                      </b>{" "}
-                      -{" "}
-                      <Link
-                        to={`/feed/${item.user.alias}`}
-                        onClick={navigateToUser}
-                      >
-                        {item.user.alias}
-                      </Link>
-                    </h2>
-                    {item.formattedDate}
-                    <br />
-                    <Post status={item} featurePath="/feed" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </InfiniteScroll>
-    </div>
-  );
+    <StatusItemScroller items={items}
+                        lastItem={lastItem}
+                        loadMore={loadMoreItems}
+                        hasMoreItems={hasMoreItems}
+                        navigateToUser={navigateToUser}
+                        featureUrl={"/feed"} />
+  )
 };
 
 export default FeedScroller;
